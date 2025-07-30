89c68eb2737cda1000dad8d03224cc4a61370c82
Danylo Liubota <daniil.lyubota@gmail.com>
2025-07-30 16:48:45 +0300

    remove comments in components

--- components/LanguageChanger/LanguageChanger.tsx
+++ components/LanguageChanger/LanguageChanger.tsx
@@ -15,14 +15,12 @@ export default function LanguageChanger() {
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newLocale = e.target.value;
 
-    // set cookie for next-i18n-router
     const days = 30;
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     const expires = date.toUTCString();
     document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
 
-    // redirect to the new locale path
     if (
       currentLocale === i18nConfig.defaultLocale &&
       !i18nConfig.prefixDefault
